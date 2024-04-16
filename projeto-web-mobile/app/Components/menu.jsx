61a6681151b36ae59037c25102d2e8dd3fd61371
Vinicius Silva Almeida <111448740+Vinicius-2003@users.noreply.github.com>
2024-04-16 09:11:30 -0300

    atualização do menu

--- projeto-web-mobile/app/Components/menu.jsx
+++ projeto-web-mobile/app/Components/menu.jsx
@@ -1,13 +1,32 @@
+"use client";
+import React, { useState } from "react";
 import Link  from "next/link"
 import Style from './menu.module.css'
+import MenuItem from "./menuItem";
+
 export default function Menu(){
+
+    const [showMenu, setShowMenu] = useState(false);
+
     return(
         <nav className={Style.menu}>
+            <button onClick={() => setShowMenu(!showMenu)}>
+                {showMenu ? '≡' : '≡'}
+            </button>
+            <h3>Menu</h3>
+            {showMenu && (
             <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/pagina1?name=Vinícius">Página 1</Link></li>
-                <li><Link href="/pagina2">Página 2</Link></li>
+                <MenuItem icon="https://super.so/icon/light/home.svg"
+                text="Home"
+                route="/" />
+                <MenuItem icon="https://super.so/icon/light/user-plus.svg"
+                text="Mackarpool"
+                route="/pagina1?name=Vinícius" />
+                <MenuItem icon="https://super.so/icon/light/frown.svg"
+                text="MackGraphs"
+                route="/pagina2" />
             </ul>
+            )}
         </nav>
     );
 }
---
+++ projeto-web-mobile/app/Components/menuItem.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import styles from "./menu.module.css"
+
+export default function MenuItem({icon, text, route}){
+
+    var contentIcon = ""
+    if(icon != null){
+        contentIcon = <img src={icon} alt={text} />
+    }
+
+    return(
+        <li>
+            <Link href={route}>{contentIcon} <span>{text}</span></Link>
+        </li>
+    )
+}
